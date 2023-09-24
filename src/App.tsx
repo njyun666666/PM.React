69f6363a58e48d39e0effb4d56333b2311d1f8a7
Jyun <njyun666666@gmail.com>
2023-09-24 19:06:13 +0800

    set shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
-function App() {
+import { ChevronRight, Loader2, Mail } from 'lucide-react';
+import { Button } from './components/ui/button';
+
+const App = () => {
+  localStorage.theme = 'dark';
+
+  if (localStorage.theme === 'light') {
+    document.documentElement.classList.remove('dark');
+  } else {
+    document.documentElement.classList.add('dark');
+  }
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-        ...
-      </button>
+      <div className="space-x-2 space-y-2 p-5">
+        <Button size={'sm'}>test</Button>
+        <Button>test</Button>
+        <Button size={'lg'}>test</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="destructive">Destructive</Button>
+        <Button variant="outline">Button</Button>
+        <Button variant="ghost">Ghost</Button>
+        <Button variant="link">Link</Button>
+        <Button variant="outline" size="icon">
+          <ChevronRight className="h-4 w-4" />
+        </Button>
+        <Button>
+          <Mail className="mr-2 h-4 w-4" /> Login with Email
+        </Button>
+        <Button disabled>
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          Please wait
+        </Button>
+      </div>
     </>
   );
-}
+};
 
 export default App;
