f760dcf2a1564dfd96a60b4fb97f3aca081c4403
Jyun <njyun666666@gmail.com>
2023-09-27 19:23:22 +0800

    fix

--- src/@types/react-i18next.d.ts
+++ src/@types/react-i18next.d.ts
@@ -3,6 +3,6 @@ import { resources } from '../i18n/config';
 
 declare module 'i18next' {
   interface CustomTypeOptions {
-    resources: (typeof resources)['zh'];
+    resources: (typeof resources)['zh-Hant'];
   }
 }
